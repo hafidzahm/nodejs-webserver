41e253316231701920b7d5b897f71f9b1e5550f8
hafizhahm <hafizhahm123@gmail.com>
2024-08-29 09:01:00 +0700

    body-request

--- server.js
+++ server.js
@@ -11,25 +11,33 @@ const requestListener = (request, response) => {
         response.end('<h1>GET RESPONSE</h1>')
     }
 
-    if (method === 'POST') {
-        response.end('<h1>POST RESPONSE</h1>')
-    }
-
-    if (method === 'PUT') {
-        response.end('<h1>PUT RESPONSE</h1>')
-    }
-    
-    if (method === 'DELETE') {
-        response.end('<h1>DELETE RESPONSE</h1>')
-    }
+    if(method === 'POST') {
+        let body = [];
+       
+        request.on('data', (chunk) => {
+          body.push(chunk);
+        });
+       
+        request.on('end', () => {
+          body = Buffer.concat(body).toString();
+          const { name } = JSON.parse(body);
+          response.end(`<h1>Hai, ${name}!</h1>`);
+        });
+      }
 };
 
 
 const server = http.createServer(requestListener);
 
-const port = 5000;
+const port = 4000;
 const host ='localhost'
+
+
 
 server.listen(port, host, () => {
     console.log(`Server berjalan pada http://${host}:${port}`)
+
 });
+
+// npm run start
+// curl -X POST -H "Content-Type: application/json" http://localhost:5000 -d "{\"name\": \"Dicoding\"}"
