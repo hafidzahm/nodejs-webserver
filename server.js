03264779008bcb6fc8cd710d76e929a395e88d1a
hafizhahm <hafizhahm123@gmail.com>
2024-08-29 09:55:21 +0700

    routing-request

--- server.js
+++ server.js
@@ -5,25 +5,62 @@ const requestListener = (request, response) => {
     response.statusCode = 200;
     //handling request
 
-    const { method } = request
+    const { url, method } = request
 
-    if (method === 'GET') {
-        response.end('<h1>GET RESPONSE</h1>')
+    if ( url === '/') {
+        //logika respon bila url bernilai '/'
+        if (method === 'GET') {
+            //jika client pake get
+            response.end('<h1>Ini adalah homepage</h1>')
+        } else {
+            //jika tidak
+            response.end(`<h1>Mohon maaf, halaman ini tidak dapat diakses pakai method ${method} request</h1>`)
+        }
+    } else if ( url === '/about') {
+        //logika respon bila url bernilai '/about'
+        if (method === 'GET') {
+            //BILA PAKAI GET
+            response.end('<h1>Halo! ini adalah halaman about.</h1>')
+        } else if (method ==='POST'){
+            //BILA PAKAI POST
+            let body = [];
+ 
+            request.on('data', (chunk) => {
+              body.push(chunk);
+            });
+    
+            request.on('end', () => {
+              body = Buffer.concat(body).toString();
+              const {name} = JSON.parse(body);
+              response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+            });
+        } else {
+            //bila tidak keduanya
+            response.end(`<h1>Ups, maaf halaman ini tidak dapat diakses pake ${method} request</h1>`)
+        }
+            
+    } else {
+        //logika respon bila url tidak bernilai keduanya
+        response.end('<h1>Halaman tidak ditemukan!</h1>')
     }
 
-    if(method === 'POST') {
-        let body = [];
+    // if (method === 'GET') {
+    //     response.end('<h1>GET RESPONSE</h1>')
+    // }
+
+    // if(method === 'POST') {
+    //     let body = [];
        
-        request.on('data', (chunk) => {
-          body.push(chunk);
-        });
+    //     request.on('data', (chunk) => {
+    //       body.push(chunk);
+    //     });
        
-        request.on('end', () => {
-          body = Buffer.concat(body).toString();
-          const { name } = JSON.parse(body);
-          response.end(`<h1>Hai, ${name}!</h1>`);
-        });
-      }
+    //     request.on('end', () => {
+    //       body = Buffer.concat(body).toString();
+    //       const { name } = JSON.parse(body);
+    //       response.end(`<h1>Hai, ${name}!</h1>`);
+    //     });
+    //   }
 };
 
 
